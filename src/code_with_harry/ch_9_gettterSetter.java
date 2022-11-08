e6489cc1718846a9116ec36a23258d365a11eab2
kumkum <kumkumrathour00@gmail.com>
2022-11-08 11:58:06 +0530

    add java

---
+++ src/code_with_harry/ch_9_gettterSetter.java
@@ -0,0 +1,13 @@
+package code_with_harry;
+   class MyStudent{
+    int id;
+    String name;
+   }
+public class ch_9_gettterSetter {
+    public static void main(String[] args){
+        MyStudent kumkum = new MyStudent();
+        kumkum.id = 553;
+        kumkum.name = "sapna";
+        System.out.println(kumkum.name);
+    }
+}
